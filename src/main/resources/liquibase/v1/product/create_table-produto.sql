3db2eeb97268736ded1f4ccb6a8ee7d6346aaee3
Jhoel Bagnasco <jhoel.bagnasco@cwi.com.br>
2021-05-27 08:08:27 -0300

    refact(liquibase): improve liquibase changelog naming

---
+++ src/main/resources/liquibase/v1/product/create_table-produto.sql
@@ -0,0 +1,11 @@
+create table produto (
+    id              bigint unsigned auto_increment,
+    titulo          varchar(100) NOT NULL,
+    preco_unitario  decimal(7,2) NOT NULL,
+    qtd_disponivel  smallint unsigned default 0,
+    descricao text  default NULL,
+    criado_em       datetime default CURRENT_TIMESTAMP,
+    alterado_em     datetime default NULL on update CURRENT_TIMESTAMP,
+    primary key (id),
+    index index_titulo (titulo)
+);

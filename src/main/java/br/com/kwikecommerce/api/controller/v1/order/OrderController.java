bfafebfd020fc235855cb0cd5f7a36c0d71e6c68
Jhoel Bagnasco <jhoel.bagnasco@cwi.com.br>
2021-08-21 21:54:38 -0300

    refact: add summary to Order operations

--- src/main/java/br/com/kwikecommerce/api/controller/v1/order/OrderController.java
+++ src/main/java/br/com/kwikecommerce/api/controller/v1/order/OrderController.java
@@ -5,6 +5,7 @@ import br.com.kwikecommerce.api.dto.request.OrderCreationRequestDto;
 import br.com.kwikecommerce.api.dto.request.OrderUpdateRequestDto;
 import br.com.kwikecommerce.api.dto.response.OrderFindingByFilterResponse;
 import br.com.kwikecommerce.api.dto.response.OrderFindingByIdResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 
@@ -16,9 +17,11 @@ import org.springframework.data.domain.Page;
 public interface OrderController {
 
     @Tag(name = "Orders")
+    @Operation(summary = "Creates a new Order with its items")
     Long create(OrderCreationRequestDto orderCreationRequestDto);
 
     @Tag(name = "Orders")
+    @Operation(summary = "Find Orders valid for given filter")
     Page<OrderFindingByFilterResponse> findByFilter(PageRequestDto pageRequestDto);
 
     @Tag(name = "Orders")
